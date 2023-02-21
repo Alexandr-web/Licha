84c4d683248a4ea38a7a9228a6bb155635224a42
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-02-21 17:25:39 +0300

    добавил 16 темных тем

--- src/js/Utils/themes/dark.js
+++ src/js/Utils/themes/dark.js
@@ -125,5 +125,397 @@ export default [
           title: { color: "#EDEAEF", },
           group: { color: "#4B4162", },
         },
+    },
+    {
+      canvas: { background: "#371F67", },
+      title: { color: "#DAC6FF", },
+      legend: { color: "#836FAB", },
+      axis: { title: { color: "#DAC6FF", }, point: { color: "#836FAB", }, },
+      grid: { color: "#4B316B", },
+      line: {
+        color: ["#9F3F7D", "#6FA1CD", "#E7877B", "#F6488A"],
+        fill: [
+          ["rgba(159,63,125,0.5)", "transparent"],
+          ["rgba(111,161,205,0.5)", "transparent"],
+          ["rgba(231,135,123,0.5)", "transparent"],
+          ["rgba(246,72,138,0.5)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#9F3F7D", "#6FA1CD", "#E7877B", "#F6488A"],
+        strokeColor: ["#371F67", "#371F67", "#371F67", "#371F67"],
+      },
+      blockInfo: {
+        window: { color: "#DAC6FF", },
+        title: { color: "#4B316B", },
+        group: { color: "#836FAB", },
+      },
+    },
+    {
+      canvas: { background: "#2A2C3B", },
+      title: { color: "#CCCCD8", },
+      legend: { color: "#7C7E83", },
+      axis: { title: { color: "#CCCCD8", }, point: { color: "#7C7E83", }, },
+      grid: { color: "#3E4156", },
+      line: {
+        color: ["#369F8F", "#2D6685", "#E48667", "#B25E8C", "#E3E77B"],
+        fill: [
+          ["rgba(54,159,143,0.3)", "transparent"],
+          ["rgba(45,102,133,0.3)", "transparent"],
+          ["rgba(228,134,103,0.3)", "transparent"],
+          ["rgba(178,94,140,0.3)", "transparent"],
+          ["rgba(227,231,123,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#369F8F", "#2D6685", "#E48667", "#B25E8C", "#E3E77B"],
+        strokeColor: ["#2A2C3B", "#2A2C3B", "#2A2C3B", "#2A2C3B", "#2A2C3B"],
+      },
+      blockInfo: {
+        window: { color: "#29384B", },
+        title: { color: "#CCCCD8", },
+        group: { color: "#7C7E83", },
+      },
+    },
+    {
+      canvas: { background: "#22252A", },
+      title: { color: "#88AFB4", },
+      legend: { color: "#317375", },
+      axis: { title: { color: "#88AFB4", }, point: { color: "#317375", }, },
+      grid: { color: "#33373E", },
+      line: {
+        color: ["#11B8BF", "#2C5A61", "#99FFFF"],
+        fill: [
+          ["rgba(17,184,191,0.3)", "transparent"],
+          ["rgba(44,90,97,0.3)", "transparent"],
+          ["rgba(153,255,255,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#11B8BF", "#2C5A61", "#99FFFF"],
+        strokeColor: [],
+      },
+      blockInfo: {
+        window: { color: "#121418", },
+        title: { color: "#88AFB4", },
+        group: { color: "#317375", },
+      },
+    },
+    {
+      canvas: { background: "#2F3245", },
+      title: { color: "#D7D9E8", },
+      legend: { color: "#828598", },
+      axis: { title: { color: "#D7D9E8", }, point: { color: "#828598", }, },
+      grid: { color: "#373B4C", },
+      line: {
+        color: ["#D4D68A", "#60AFA6", "#5A7DCF", "#D580E7"],
+        fill: [
+          ["rgba(212,214,138,0.3)", "transparent"],
+          ["rgba(96,175,166,0.3)", "transparent"],
+          ["rgba(90,125,207,0.3)", "transparent"],
+          ["rgba(213,128,231,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#D4D68A", "#60AFA6", "#5A7DCF", "#D580E7"],
+        strokeColor: ["#2F3245", "#2F3245", "#2F3245", "#2F3245"],
+      },
+      blockInfo: {
+        window: { color: "#262938", },
+        title: { color: "#D7D9E8", },
+        group: { color: "#828598", },
+      },
+    },
+    {
+      canvas: { background: "#3C4256", },
+      title: { color: "#FFF4F7", },
+      legend: { color: "#B3B7BE", },
+      axis: { title: { color: "#FFF4F7", }, point: { color: "#B3B7BE", }, },
+      grid: { color: "#4C5061", },
+      line: {
+        color: ["#FCFFFF", "#85EDFF", "#FC8EFF"],
+        fill: [
+          ["rgba(252,255,255,0.3)", "transparent"],
+          ["rgba(133,237,255,0.3)", "transparent"],
+          ["rgba(252,142,255,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#B3B7BE", "#A6F2FF", "#FDBBFF"],
+        strokeColor: ["#FCFFFF", "#85EDFF", "#FC8EFF"],
+      },
+      blockInfo: {
+        window: { color: "#2D303E", },
+        title: { color: "#FFF4F7", },
+        group: { color: "#B3B7BE", },
+      },
+    },
+    {
+      canvas: { background: "#3D404F", },
+      title: { color: "#E1E1F0", },
+      legend: { color: "#C8C8D5", },
+      axis: { title: { color: "#E1E1F0", }, point: { color: "#C8C8D5", }, },
+      grid: { color: "#484B5A", },
+      line: { color: ["#6CB1F8", "#F7D076", "#E9945A", "#CFD4DA"], },
+      cap: { color: ["#6CB1F8", "#F7D076", "#E9945A", "#CFD4DA"], },
+      blockInfo: {
+        window: { color: "#2D303E", },
+        title: { color: "#E1E1F0", },
+        group: { color: "#C8C8D5", },
+      },
+    },
+    {
+      canvas: { background: "#1F212E", },
+      title: { color: "#C6CFE4", },
+      legend: { color: "#747985", },
+      axis: { title: { color: "#C6CFE4", }, point: { color: "#747985", }, },
+      grid: { color: "#2D2F3C", },
+      line: {
+        color: ["#D188F5", "#3E99E8", "#9244E9", "#EDDFF6"],
+        fill: [
+          ["rgba(229,181,239,0.3)", "transparent"],
+          ["rgba(77,132,236,0.3)", "transparent"],
+          ["rgba(146,68,233,0.3)", "transparent"],
+          ["rgba(237,223,246,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#D188F5", "#3E99E8", "#9244E9", "#EDDFF6"],
+        strokeColor: ["#1F212E", "#1F212E", "#1F212E", "#1F212E"],
+      },
+      blockInfo: {
+        window: { color: "#0F111E", },
+        title: { color: "#C6CFE4", },
+        group: { color: "#747985", },
+      },
+    },
+    {
+      canvas: { background: "#171717", },
+      title: { color: "#D9D9D9", },
+      legend: { color: "#BBBBBB", },
+      axis: { title: { color: "#D9D9D9", }, point: { color: "#BBBBBB", }, },
+      grid: { color: "#2C2C2C", },
+      line: {
+        color: ["#C46BD1", "#8157E3", "#74E8BD", "#FCB72A"],
+        fill: [
+          ["rgba(196,107,209,0.3)", "transparent"],
+          ["rgba(129,87,227,0.3)", "transparent"],
+          ["rgba(116,232,189,0.3)", "transparent"],
+          ["rgba(252,183,42,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#C46BD1", "#8157E3", "#74E8BD", "#FCB72A"],
+        strokeColor: ["#171717", "#171717", "#171717", "#171717"],
+      },
+      blockInfo: {
+        window: { color: "#000000", },
+        title: { color: "#D9D9D9", },
+        group: { color: "#BBBBBB", },
+      },
+    },
+    {
+      canvas: { background: "#2A2430", },
+      title: { color: "#C8C0D0", },
+      legend: { color: "#938D99", },
+      axis: { title: { color: "#C8C0D0", }, point: { color: "#938D99", }, },
+      grid: { color: "#38323E", },
+      line: {
+        color: ["#0FE5F3", "#138CED", "#E548E3", "#5A4DD3", "#4DD35A"],
+        fill: [
+          ["rgba(15,229,243,0.3)", "transparent"],
+          ["rgba(19,140,237,0.3)", "transparent"],
+          ["rgba(229,72,227,0.3)", "transparent"],
+          ["rgba(90,77,211,0.3)", "transparent"],
+          ["rgba(77,211,90,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#0FE5F3", "#138CED", "#E548E3", "#5A4DD3", "#4DD35A"],
+        strokeColor: ["#2A2430", "#2A2430", "#2A2430", "#2A2430", "#2A2430"],
+      },
+      blockInfo: {
+        window: { color: "#100D12", },
+        title: { color: "#C8C0D0", },
+        group: { color: "#938D99", },
+      },
+    },
+    {
+      canvas: { background: "#383B4A", },
+      title: { color: "#DBD9E6", },
+      legend: { color: "#C3C2CD", },
+      axis: { title: { color: "#DBD9E6", }, point: { color: "#C3C2CD", }, },
+      grid: { color: "#5C6177", },
+      line: {
+        color: ["#52CFE7", "#1E77E0", "#EA9F41", "#0A8792", "#94398C"],
+        fill: [
+          ["rgba(82,207,231,0.3)", "transparent"],
+          ["rgba(30,119,224,0.3)", "transparent"],
+          ["rgba(234,159,65,0.3)", "transparent"],
+          ["rgba(10,135,146,0.3)", "transparent"],
+          ["rgba(148,57,140,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#52CFE7", "#1E77E0", "#EA9F41", "#0A8792", "#94398C"],
+        strokeColor: ["#383B4A", "#383B4A", "#383B4A", "#383B4A", "#383B4A"],
+      },
+      blockInfo: {
+        window: { color: "#343746", },
+        title: { color: "#DBD9E6", },
+        group: { color: "#C3C2CD", },
+      },
+    },
+    {
+      canvas: { background: "#130F14", },
+      title: { color: "#ADADB2", },
+      legend: { color: "#8A8A8E", },
+      axis: { title: { color: "#ADADB2", }, point: { color: "#8A8A8E", }, },
+      grid: { color: "#2C2E38", },
+      line: {
+        color: ["#A62D33", "#56A249", "#DBBD55", "#0A8792", "#863CB0"],
+        fill: [
+          ["rgba(166,45,51,0.3)", "transparent"],
+          ["rgba(86,162,73,0.3)", "transparent"],
+          ["rgba(234,159,65,0.3)", "transparent"],
+          ["rgba(10,135,146,0.3)", "transparent"],
+          ["rgba(134,60,176,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#A62D33", "#56A249", "#DBBD55", "#0A8792", "#863CB0"],
+        strokeColor: ["#130F14", "#130F14", "#130F14", "#130F14", "#130F14"],
+      },
+      blockInfo: {
+        window: { color: "#050506", },
+        title: { color: "#ADADB2", },
+        group: { color: "#8A8A8E", },
+      },
+    },
+    {
+      canvas: { background: "#212338", },
+      title: { color: "#97B7BF", },
+      legend: { color: "#7F99A0", },
+      axis: { title: { color: "#97B7BF", }, point: { color: "#7F99A0", }, },
+      grid: { color: "#40435C", },
+      line: {
+        color: ["#FB6F74", "#F2A652", "#CB4C8C", "#17C5CB", "#25F6B4"],
+        fill: [
+          ["rgba(251,111,116,0.3)", "transparent"],
+          ["rgba(242,166,82,0.3)", "transparent"],
+          ["rgba(203,76,140,0.3)", "transparent"],
+          ["rgba(23,197,203,0.3)", "transparent"],
+          ["rgba(37,246,180,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#FB6F74", "#F2A652", "#CB4C8C", "#17C5CB", "#25F6B4"],
+        strokeColor: ["#212338", "#212338", "#212338", "#212338", "#212338"],
+      },
+      blockInfo: {
+        window: { color: "#171827", },
+        title: { color: "#97B7BF", },
+        group: { color: "#7F99A0", },
+      },
+    },
+    {
+      canvas: { background: "#0E0A01", },
+      title: { color: "#C4B5D0", },
+      legend: { color: "#9F93A9", },
+      axis: { title: { color: "#C4B5D0", }, point: { color: "#9F93A9", }, },
+      grid: { color: "#1B100D", },
+      line: {
+        color: ["#7C51AB", "#AAA8A9", "#C89DFA"],
+        fill: [
+          ["rgba(124,81,171,0.3)", "transparent"],
+          ["rgba(170,168,169,0.3)", "transparent"],
+          ["rgba(200,157,250,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#7C51AB", "#AAA8A9", "#C89DFA"],
+        strokeColor: ["#0E0A01", "#0E0A01", "#0E0A01"],
+      },
+      blockInfo: {
+        window: { color: "#000000", },
+        title: { color: "#C4B5D0", },
+        group: { color: "#9F93A9", },
+      },
+    },
+    {
+      canvas: { background: "#000000", },
+      title: { color: "#C5C5C5", },
+      legend: { color: "#ACACAC", },
+      axis: { title: { color: "#C5C5C5", }, point: { color: "#ACACAC", }, },
+      grid: { color: "#1E1E1E", },
+      line: {
+        color: ["#FC1819", "#FFF2F0", "#333532", "#C0BFC0", "#99342F"],
+        fill: [
+          ["rgba(252,24,25,0.3)", "transparent"],
+          ["rgba(255,242,240,0.3)", "transparent"],
+          ["rgba(51,53,50,0.3)", "transparent"],
+          ["rgba(192,191,192,0.3)", "transparent"],
+          ["rgba(153,52,47,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#FC1819", "#FFF2F0", "#333532", "#C0BFC0", "#99342F"],
+        strokeColor: ["#000000", "#000000", "#000000", "#000000", "#000000"],
+      },
+      blockInfo: {
+        window: { color: "#1E1E1E", },
+        title: { color: "#C5C5C5", },
+        group: { color: "#ACACAC", },
+      },
+    },
+    {
+      canvas: { background: "#193565", },
+      title: { color: "#E7EDFF", },
+      legend: { color: "#CCD1E1", },
+      axis: { title: { color: "#E7EDFF", }, point: { color: "#CCD1E1", }, },
+      grid: { color: "#102241", },
+      line: {
+        color: ["#9E51D0", "#6485E2", "#01A8D0", "#0154CE"],
+        fill: [
+          ["rgba(158,81,208,0.3)", "transparent"],
+          ["rgba(100,133,226,0.3)", "transparent"],
+          ["rgba(1,168,208,0.3)", "transparent"],
+          ["rgba(1,84,206,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#9E51D0", "#6485E2", "#01A8D0", "#0154CE"],
+        strokeColor: ["#193565", "#193565", "#193565", "#193565"],
+      },
+      blockInfo: {
+        window: { color: ["#0A1427", "#152D56"], },
+        title: { color: "#E7EDFF", },
+        group: { color: "#CCD1E1", },
+      },
+    },
+    {
+      canvas: { background: "#211E3D", },
+      title: { color: "#FDFEF8", },
+      legend: { color: "#D5D6D1", },
+      axis: { title: { color: "#FDFEF8", }, point: { color: "#D5D6D1", }, },
+      grid: { color: "#373356", },
+      line: {
+        color: ["#F326A3", "#9153F4", "#F2A543", "#FFFCFF"],
+        fill: [
+          ["rgba(243,38,163,0.3)", "transparent"],
+          ["rgba(145,83,244,0.3)", "transparent"],
+          ["rgba(242,165,67,0.3)", "transparent"],
+          ["rgba(255,252,255,0.3)", "transparent"]
+        ],
+      },
+      cap: {
+        color: ["#F326A3", "#9153F4", "#F2A543", "#FFFCFF"],
+        strokeColor: ["#211E3D", "#211E3D", "#211E3D", "#211E3D"],
+      },
+      blockInfo: {
+        window: { color: ["#121021", "#151327"], },
+        title: { color: "#FDFEF8", },
+        group: { color: "#D5D6D1", },
+      },
     }
 ];
