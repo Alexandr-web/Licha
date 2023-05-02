394270ab8171ec6df8d6dd1c1eb15d0577bedda7
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-05-02 19:53:53 +0300

    решил проблему с отрицательными значениями

--- src/ts/helpers/getRoundedNumber.ts
+++ src/ts/helpers/getRoundedNumber.ts
@@ -1,10 +1,14 @@
+import ifTrueThenOrElse from "./ifTrueThenOrElse";
+
 /**
  * Округляет число до "красивого" значения
  * @param {number} num Содержит массив чисел
  * @returns {number}
  */
 export default function (num: number): number {
-    const count: number = 10 ** (num.toString().length - 1);
+    const n: number = Math.abs(num);
+    const count: number = 10 ** (Math.abs(n).toString().length - 1);
+    const nextNum: number = n % count ? n + count - (n % count) : n;
 
-    return num % count ? num + count - (num % count) : num;
+    return ifTrueThenOrElse(num < 0, -nextNum, nextNum);
 }
